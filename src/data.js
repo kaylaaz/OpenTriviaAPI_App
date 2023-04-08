3f8c3a375d1931489704c68a5125fceb9033c033
Kayla Z <kaylaz98@berkeley.edu>
2023-04-08 15:15:06 -0700

    displaying questions

---
+++ src/data.js
@@ -0,0 +1,13 @@
+export const musicData = [
+    {
+        "response_code": 0,
+        "results": [{ "category": "Entertainment: Music", "type": "boolean", "difficulty": "easy", "question": "The music group Daft Punk got their name from a negative review they recieved.", "correct_answer": "True", "incorrect_answers": ["False"] }, { "category": "Entertainment: Music", "type": "boolean", "difficulty": "easy", "question": "The music video to The Buggle&#039;s &quot;Video Killed the Radio Star&quot; was the first music video to broadcast on MTV.", "correct_answer": "True", "incorrect_answers": ["False"] }, { "category": "Entertainment: Music", "type": "boolean", "difficulty": "easy", "question": "Eurobeat is primarily an Italian genre of music.", "correct_answer": "True", "incorrect_answers": ["False"] }, { "category": "Entertainment: Music", "type": "boolean", "difficulty": "easy", "question": "The alternative rock band, They Might Be Giants, released their album &#039;Flood&#039; in 1990. ", "correct_answer": "True", "incorrect_answers": ["False"] }, { "category": "Entertainment: Music", "type": "boolean", "difficulty": "easy", "question": "Daft Punk originated in France.", "correct_answer": "True", "incorrect_answers": ["False"] }, { "category": "Entertainment: Music", "type": "boolean", "difficulty": "easy", "question": "Scatman John&#039;s real name was John Paul Larkin.", "correct_answer": "True", "incorrect_answers": ["False"] }, { "category": "Entertainment: Music", "type": "boolean", "difficulty": "easy", "question": "Michael Jackson wrote The Simpsons song &quot;Do the Bartman&quot;.", "correct_answer": "False", "incorrect_answers": ["True"] }, { "category": "Entertainment: Music", "type": "boolean", "difficulty": "easy", "question": "In 1993, Prince changed his name to an unpronounceable symbol because he was unhappy with his contract with Warner Bros.", "correct_answer": "True", "incorrect_answers": ["False"] }, { "category": "Entertainment: Music", "type": "boolean", "difficulty": "easy", "question": "The 2011 movie &quot;The Adventures of Tintin&quot; was directed by Steven Spielberg.", "correct_answer": "True", "incorrect_answers": ["False"] }, { "category": "Entertainment: Music", "type": "boolean", "difficulty": "easy", "question": "Lead Singer Rivers Cuomo of American rock band Weezer attended Harvard.", "correct_answer": "True", "incorrect_answers": ["False"] }]
+    }
+]
+
+//category: Entertainment: Music
+//type of questions: True/False (Boolean)
+//level of difficulty: easy
+//question:
+//Answer: True or False
+//Incorrect answer: T or F (aka opposite answer of "Answer")
